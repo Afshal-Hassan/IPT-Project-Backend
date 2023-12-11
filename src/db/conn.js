baf2a1c01f5d8f18525208b3547332a649e3b538
Network Admin <btsadmin@DEV-16.BARITECHSOL.COM>
2023-12-11 13:35:53 +0500

    first commit

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,9 +1,11 @@
-const mongoose = require("mongoose") 
+const mongoose = require("mongoose");
 
-
-mongoose.connect(process.env.MONGODB_URL + process.env.MONGODB_DATABASE).then(() => {
-    console.log("Database connected")
-}).catch((error) => {
-    console.log("No database connection")
-    console.log(process.env.MONGODB_URL)
-})
+mongoose
+  .connect(process.env.MONGODB_URL_AND_DATABASE)
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((error) => {
+    console.log("No database connection");
+    console.log(process.env.MONGODB_URL);
+  });
